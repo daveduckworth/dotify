b0c5d792af37473aa36654c04fcc40652af400c7
Dave Duckworth <dave.duckworth@me.com>
2016-12-02 19:47:10 +0000

    Spike out artist routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,31 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$router->get('/albums/{id}', function ($id) {
+	$spotify = new \GuzzleHttp\Client();
 
-Route::get('/', function () {
-    return view('welcome');
+	return $spotify->get('https://api.spotify.com/v1/artists/' . $id);
+});
+
+$router->get('/artists/{id}', function ($id) {
+	$spotify = new \GuzzleHttp\Client();
+
+	return $spotify->get('https://api.spotify.com/v1/artists/' . $id);
+});
+
+$router->get('/artists/{id}/albums', function ($id) {
+	$spotify = new \GuzzleHttp\Client();
+
+	return $spotify->get('https://api.spotify.com/v1/artists/' . $id . '/albums');
+});
+
+$router->get('/artists/{id}/top-tracks', function ($id) {
+	$spotify = new \GuzzleHttp\Client();
+
+	return $spotify->get('https://api.spotify.com/v1/artists/' . $id . '/top-tracks?country=GB');
+});
+
+$router->get('/artists/{id}/related-artists', function ($id) {
+	$spotify = new \GuzzleHttp\Client();
+
+	return $spotify->get('https://api.spotify.com/v1/artists/' . $id . '/related-artists');
 });
