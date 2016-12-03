a626086001033b18b6713a243186b70308d6af0e
Dave Duckworth <dave.duckworth@me.com>
2016-12-03 00:36:04 +0000

    Refactor to abstract API endpoints

--- app/Repositories/ArtistRepository.php
+++ app/Repositories/ArtistRepository.php
@@ -2,31 +2,32 @@
 
 namespace App\Repositories;
 
-use GuzzleHttp\Client;
+use App\Spotify\SpotifyClient;
 
 class ArtistRepository
 {
-	public function __construct(Client $spotify)
+	public function __construct(SpotifyClient $spotify)
 	{
 		$this->spotify = $spotify;
 	}
 
-	public function getById($id) {
-		return $this->spotify->get('artists/' . $id);
+	public function getById($id)
+	{
+		return $this->spotify->getArtistById($id);
 	}
 
-	public function getAlbums($artistId)
+	public function getAlbums($artistId, $country = 'GB')
 	{
-		return $this->spotify->get('artists/' . $artistId . '/albums');
+		return $this->spotify->getAlbumsByArtistId($artistId, $country);
 	}
 
 	public function getTopTracks($artistId, $country = 'GB')
 	{
-		return $this->spotify->get('artists/' . $artistId . '/top-tracks?country=' . $country);
+		return $this->spotify->getTopTracksByArtistId($id, $country);
 	}
 
 	public function getRelatedArtists($artistId)
 	{
-		return $this->spotify->get('artists/' . $artistId . '/related-artists');
+		return $this->spotify->getRelatedArtistsByArtistId($artistId);
 	}
 }
