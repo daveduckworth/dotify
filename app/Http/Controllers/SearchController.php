3a4eda7049a06d67aa25d7a37d4b73bb0d227bb2
Dave Duckworth <dave.duckworth@me.com>
2016-12-03 04:30:13 +0000

    Add search controller

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\SearchRepository;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function __construct(SearchRepository $search)
+    {
+        $this->search = $search;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return view('search.index', [
+            'artists' => $this->search->artists($request->get('query'))
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new search.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('search.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
