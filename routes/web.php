3d4f396c0d77b1590e98f77d1c1cd749c85131e2
Dave Duckworth <dave.duckworth@me.com>
2016-12-03 00:47:33 +0000

    Remove surplus routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,3 @@
 <?php
 
 $router->get('/artists/{id}', 'ArtistController@show');
-
-$router->get('/artists/{id}/albums', function ($id) {
-	//
-});
-
-$router->get('/artists/{id}/top-tracks', function ($id) {
-	//
-});
-
-$router->get('/artists/{id}/related-artists', function ($id) {
-	//
-});
