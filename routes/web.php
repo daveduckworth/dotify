31364d3157bf7377d04063fa72bc09e9dfac2116
Dave Duckworth <dave.duckworth@me.com>
2016-12-03 00:36:35 +0000

    Simplify routes and begin using controllers

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,15 @@
 <?php
 
-$router->get('/albums/{id}', function ($id) {
-	$spotify = new \GuzzleHttp\Client();
-
-	return $spotify->get('https://api.spotify.com/v1/artists/' . $id);
-});
-
-$router->get('/artists/{id}', function ($id) {
-	$spotify = new \GuzzleHttp\Client();
-
-	return $spotify->get('https://api.spotify.com/v1/artists/' . $id);
-});
+$router->get('/artists/{id}', 'ArtistController@show');
 
 $router->get('/artists/{id}/albums', function ($id) {
-	$spotify = new \GuzzleHttp\Client();
-
-	return $spotify->get('https://api.spotify.com/v1/artists/' . $id . '/albums');
+	//
 });
 
 $router->get('/artists/{id}/top-tracks', function ($id) {
-	$spotify = new \GuzzleHttp\Client();
-
-	return $spotify->get('https://api.spotify.com/v1/artists/' . $id . '/top-tracks?country=GB');
+	//
 });
 
 $router->get('/artists/{id}/related-artists', function ($id) {
-	$spotify = new \GuzzleHttp\Client();
-
-	return $spotify->get('https://api.spotify.com/v1/artists/' . $id . '/related-artists');
+	//
 });
