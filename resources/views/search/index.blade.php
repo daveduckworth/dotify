580657a0c34c257f4515efaae90fb8a9a72c3096
Dave Duckworth <dave.duckworth@me.com>
2016-12-03 04:39:05 +0000

    Add album results

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -6,9 +6,9 @@
 		<h1 class="f3 fw4 pa4 mv0">Search</h1>
 	</article>
 
-	<article class="cf">
+	<article class="cf bb b--black-10">
 		<div class="fl w-100 w-50-ns tc">
-			<article class="pa1 pa3-ns tc bb b--black-10">
+			<article class="pa1 pa3-ns tc">
 				<h2 class="f3 fw4 pa4 mv0">Artists</h2>
 
 				<section class="cf w-100 pa2-ns">
@@ -34,7 +34,31 @@
 		</div>
 
 		<div class="fl w-100 w-50-ns tc">
-			<h1>Column Two</h1>
+			<article class="pa1 pa3-ns tc">
+				<h2 class="f3 fw4 pa4 mv0">Albums</h2>
+
+				<section class="cf w-100 pa2-ns">
+					@foreach ($albums->albums->items as $album)
+						<article class="fl w-100 w-50-m w-25-ns pa2-ns">
+							<div class="aspect-ratio aspect-ratio--1x1">
+								<a href="/albums/{{ $album->id }}" class="dim">
+									<img style="background-image:url({{ $album->images[1]->url }});"
+										 class="db bg-center cover aspect-ratio--object" />
+								</a>
+							</div>
+
+							<a href="/albums/{{ $album->id }}" class="ph2 ph0-ns pb3 link db dim">
+								<h3 class="f5 f4-ns mb0 black-90">{{ $album->name }}</h3>
+								<h3 class="f6 f5 fw4 mt2 black-60">{{ $album->artists[0]->name }}</h3>
+							</a>
+						</article>
+
+						@break($loop->iteration == 8)
+					@endforeach
+				</section>
+			</article>
+			
+
 		</div>
 	</article>
 
