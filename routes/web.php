b13a291ad67332bc8f510120474fe3acd8e0c48d
Dave Duckworth <dave.duckworth@me.com>
2016-12-03 04:30:02 +0000

    Add search routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@ $router->get('/albums/{id}', 'AlbumController@show');
 
 $router->get('/artists/{id}', 'ArtistController@show');
 
+$router->get('/search/create', 'SearchController@create');
+$router->get('/search', 'SearchController@index');
+
 $router->get('/tracks/{id}', 'TrackController@show');
